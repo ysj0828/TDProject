22d990e4a51807aab4ac087eeadcb9957db7ad12
TDProject <52316013+marshmello-std@users.noreply.github.com>
2021-04-08 02:25:50 +0900

    Tilemap, Waypoint Loop

---
+++ Tower Defense/Assets/EnemyScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed = 1000f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.5f)
+        {
+            if (waypointIndex >= waypoints.points.Length)
+            {
+                waypointIndex = 0;
+                getNextWaypoint();
+            }
+
+            else
+            {
+                getNextWaypoint();
+            }
+        }
+    }
+
+    void getNextWaypoint()
+    {
+        waypointIndex++;
+        target = waypoints.points[waypointIndex - 1];
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        var dir = waypoints.points[waypointIndex - 1].position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
