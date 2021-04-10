ee6366e9f1815cbc0e74e19df1501c84e142534d
TDProject <52316013+marshmello-std@users.noreply.github.com>
2021-04-11 04:42:04 +0900

    Reworked everything

--- Tower Defense/Assets/EnemyScript.cs
+++ Tower Defense/Assets/EnemyScript.cs
@@ -4,47 +4,167 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    public float speed = 1000f;
+    public float speed = 1f;
 
     private Transform target;
-    private int waypointIndex = 0;
+    private int waypointIndexPoint1 = 0;
+    private int waypointIndexPoint2 = 1;
+    private int waypointIndexPoint3 = 2;
+    private int waypointIndexPoint4 = 3;
+
+    private bool spawnedAtPoint1 = false;
+    private bool spawnedAtPoint2 = false;
+    private bool spawnedAtPoint3 = false;
+    private bool spawnedAtPoint4 = false;
 
     void Start()
     {
-        target = waypoints.points[0];
+        //스폰 장소에 따라 이니셜 타겟 웨이포인트 설정
+        if (transform.position == GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<WaveSpawner>().spawnPoint1.position)
+        {
+            target = waypoints.points[waypointIndexPoint1];
+            spawnedAtPoint1 = true;
+        }
+
+        else if (transform.position == GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<WaveSpawner>().spawnPoint2.position)
+        {
+            target = GameObject.FindGameObjectWithTag("TempWaypoint1").transform;
+            spawnedAtPoint2 = true;
+        }
+
+        else if (transform.position == GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<WaveSpawner>().spawnPoint3.position)
+        {
+            target = waypoints.points[waypointIndexPoint3];
+            spawnedAtPoint3 = true;
+        }
+
+        else if (transform.position == GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<WaveSpawner>().spawnPoint4.position)
+        {
+            target = GameObject.FindGameObjectWithTag("TempWaypoint2").transform;
+            spawnedAtPoint4 = true;
+        }
     }
 
     void Update()
     {
+        //이동 & way point loop 
         Vector2 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        if (Vector2.Distance(transform.position, target.position) <= 0.5f)
+        if (Vector2.Distance(transform.position, target.position) <= 0.05f && spawnedAtPoint1)
         {
-            if (waypointIndex >= waypoints.points.Length)
+            if (waypointIndexPoint1 >= waypoints.points.Length)
             {
-                waypointIndex = 0;
-                getNextWaypoint();
+                waypointIndexPoint1 = 0;
+                getNextWaypoint1();
             }
 
             else
             {
-                getNextWaypoint();
+                getNextWaypoint1();
+            }
+        }
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.05f && spawnedAtPoint2)
+        {
+            if (waypointIndexPoint2 >= waypoints.points.Length)
+            {
+                waypointIndexPoint2 = 0;
+                getNextWaypoint2();
+            }
+
+            else
+            {
+                getNextWaypoint2();
+            }
+        }
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.05f && spawnedAtPoint3)
+        {
+            if (waypointIndexPoint3 >= waypoints.points.Length)
+            {
+                waypointIndexPoint3 = 0;
+                getNextWaypoint3();
+            }
+
+            else
+            {
+                getNextWaypoint3();
+            }
+        }
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.05f && spawnedAtPoint4)
+        {
+            if (waypointIndexPoint4 >= waypoints.points.Length)
+            {
+                waypointIndexPoint4 = 0;
+                getNextWaypoint4();
+            }
+
+            else
+            {
+                getNextWaypoint4();
             }
         }
     }
 
-    void getNextWaypoint()
+    //다음 way point 호출 함수
+    void getNextWaypoint1()
     {
-        waypointIndex++;
-        target = waypoints.points[waypointIndex - 1];
-        Rotate();
+        waypointIndexPoint1++;
+        target = waypoints.points[waypointIndexPoint1 - 1];
+        Rotate1();
     }
 
-    void Rotate()
+    void getNextWaypoint2()
     {
-        var dir = waypoints.points[waypointIndex - 1].position - transform.position;
+        waypointIndexPoint2++;
+        target = waypoints.points[waypointIndexPoint2 - 1];
+        Rotate2();
+    }
+
+    void getNextWaypoint3()
+    {
+        waypointIndexPoint3++;
+        target = waypoints.points[waypointIndexPoint3 - 1];
+        Rotate3();
+    }
+
+    void getNextWaypoint4()
+    {
+        waypointIndexPoint4++;
+        target = waypoints.points[waypointIndexPoint4 - 1];
+        Rotate4();
+    }
+
+
+    //enemy prefab 회전
+    void Rotate1()
+    {
+        var dir = waypoints.points[waypointIndexPoint1 - 1].position - transform.position;
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
+
+    void Rotate2()
+    {
+        var dir = waypoints.points[waypointIndexPoint2 - 1].position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Rotate3()
+    {
+        var dir = waypoints.points[waypointIndexPoint3 - 1].position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Rotate4()
+    {
+        var dir = waypoints.points[waypointIndexPoint4 - 1].position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
 }
--- Tower Defense/Assets/WaveSpawner.cs
+++ Tower Defense/Assets/WaveSpawner.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
-    public Transform spawnLocation;
+
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    public Transform spawnPoint3;
+    public Transform spawnPoint4;
 
     public float waveCountDown = 10f;
     private float counter = 2f;
 
     private int waveIndex = 0;
-    private int enemyPerWave = 10;
+    private int enemyPerWave = 1;
 
-
+    public Text waveCountdownText;
 
     void Update()
     {
@@ -24,8 +29,11 @@ public class WaveSpawner : MonoBehaviour
         }
 
         counter -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Floor(counter).ToString();
     }
 
+    //0.5초 간격으로 SpawnEnemy 함수 호출 & 웨이브 + 1
     IEnumerator SpawnWave()
     {
         waveIndex++;
@@ -36,8 +44,12 @@ public class WaveSpawner : MonoBehaviour
         }
     }
 
+    //enemy prefab 생성
     void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, spawnLocation.position, spawnLocation.rotation);
+        Instantiate(enemyPrefab, spawnPoint1.position, spawnPoint1.rotation);
+        Instantiate(enemyPrefab, spawnPoint2.position, spawnPoint2.rotation);
+        Instantiate(enemyPrefab, spawnPoint3.position, spawnPoint3.rotation);
+        Instantiate(enemyPrefab, spawnPoint4.position, spawnPoint4.rotation);
     }
 }
